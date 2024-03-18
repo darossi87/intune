f724dfc590bba1c7c253d478fca29bebbfae7ee6
David Rossi <45303117+darossi87@users.noreply.github.com>
2024-03-18 14:03:19 -0400

    Update OrgfileMove.sh

--- OrgfileMove.sh
+++ OrgfileMove.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # URL of the file to download
-file_url="URL_HERE WHERE YOUR ORG FILE IS LOCATED"
+file_url="URL_HERE"
 
 # Destination directory
 destination_dir="/opt/cisco/secureclient/umbrella/"
@@ -11,14 +11,14 @@ sudo mkdir -p "$destination_dir"
 
 # Download the file
 echo "Downloading file..."
-sudo curl -L -o "$destination_dir/file_name.extension" "$file_url"
+sudo curl -L -o "$destination_dir/Orginfo.json" "$file_url"
 
 # Check if download was successful
 if [ $? -eq 0 ]; then
     echo "File downloaded successfully."
 
     # Move the downloaded file to the destination directory
-    sudo mv "$destination_dir/file_name.extension" "$destination_dir"
+    sudo mv "$destination_dir/Orginfo.json" "$destination_dir"
 
     # Check if move was successful
     if [ $? -eq 0 ]; then
